e886530e600502d4986b4ec34d7916224c0ce5ce
Renan <renan.auer@gmail.com>
2023-09-28 01:42:54 -0300

    Create movie model and migrate

--- database/migrations/2023_09_27_234955_create_movies_table.php
+++ database/migrations/2023_09_27_234955_create_movies_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->integer('year');
+            $table->string('title');
+            $table->string('studios');
+            $table->string('producers');
+            $table->boolean('winner');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('movies');
     }
 };
