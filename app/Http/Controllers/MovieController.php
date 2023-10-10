d4a715368fc644854c0899e37f24076b414ac2d4
Renan <renan.auer@gmail.com>
2023-10-10 15:18:21 -0300

    Fix getIntervals function

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -11,16 +11,16 @@ class MovieController extends Controller
     function getMaxAndMinIntervalWinProducers()
     {
         $intervals = $this->getIntervals();
-
         return array(
-            "min" => $this->getMinIntervalWinProducers($intervals),
-            "max" => $this->getMaxIntervalWinProducers($intervals)
+            "min" => $this->getMinIntervalsWinProducers($intervals),
+            "max" => $this->getMaxIntervalsWinProducers($intervals)
         );
     }
 
     //Function that checks the intervals of each winning producer
-    private function getIntervals() {
-        $movies = Movie::all();
+    private function getIntervals()
+    {
+        $movies = Movie::orderBy('year')->get();
         $producers = Producer::all();
 
         $intervals = [];
@@ -28,18 +28,20 @@ class MovieController extends Controller
         foreach ($producers as $producer) {
             foreach ($movies as $movie) {
                 if ($movie->wasProducedByProducer($producer) && $movie->winner == true) {
-                    if (isset($intervals[$producer->id])) {
-                        $previousWin = $intervals[$producer->id]['previousWin'];
-                        $interval = intval($movie->year) - intval($previousWin);
-                        $intervals[$producer->id]['interval'] = $interval;
-                        $intervals[$producer->id]['followingWin'] = $movie->year;
-                    } else {
 
-                        $intervals[$producer->id] = [
-                            'previousWin' =>  $movie->year,
-                            'producer' =>  $producer->name,
-                        ];
+                    foreach ($intervals as &$interval) {
+                       
+                        if ($interval["producer"] == $producer->name && !isset($interval["interval"])) {
+                            $intervalNumber = intval($movie->year) - intval($interval['previousWin']);
+                            $interval['interval'] = $intervalNumber;
+                            $interval['followingWin'] = $movie->year;
+                        }
                     }
+
+                    array_push($intervals, [
+                        'previousWin' =>  $movie->year,
+                        'producer' =>  $producer->name,
+                    ]);
                 }
             }
         }
@@ -47,33 +49,45 @@ class MovieController extends Controller
         return $intervals;
     }
 
-    private function getMinIntervalWinProducers($intervals)
+    private function getMinIntervalsWinProducers($intervals)
     {
-        $minInterval = null;
+        $minIntervals = [];
+        $minIntervalNumber = null;
+
         foreach ($intervals as $interval) {
-            if (isset($interval["interval"]) && !$minInterval) {
-                $minInterval = $interval;
-            }
-            if (isset($interval["interval"]) && $interval["interval"] < $minInterval["interval"]) {
-                $minInterval = $interval;
+            if (isset($interval["interval"]) && !$minIntervalNumber) {
+                $minIntervalNumber = $interval["interval"];
+                array_push($minIntervals, $interval);
+            } else if (isset($interval["interval"]) && $interval["interval"] < $minIntervalNumber) {
+                $minIntervalNumber = $interval["interval"];
+                $minIntervals = [];
+                array_push($minIntervals, $interval);
+            }  else if (isset($interval["interval"]) && $interval["interval"] == $minIntervalNumber) {
+                array_push($minIntervals, $interval);
             }
         }
 
-        return $minInterval;
+        return $minIntervals;
     }
 
-    private function getMaxIntervalWinProducers($intervals)
+    private function getMaxIntervalsWinProducers($intervals)
     {
-        $maxInterval = null;
+        $maxIntervals = [];
+        $maxIntervalNumber = null;
+
         foreach ($intervals as $interval) {
-            if (isset($interval["interval"]) && !$maxInterval) {
-                $maxInterval = $interval;
-            }
-            if (isset($interval["interval"]) && $interval["interval"] > $maxInterval["interval"]) {
-                $maxInterval = $interval;
+            if (isset($interval["interval"]) && !$maxIntervalNumber) {
+                $maxIntervalNumber = $interval["interval"];
+                array_push($maxIntervals, $interval);
+            } else if (isset($interval["interval"]) && $interval["interval"] > $maxIntervalNumber) {
+                $maxIntervalNumber = $interval["interval"];
+                $maxIntervals = [];
+                array_push($maxIntervals, $interval);
+            } else if (isset($interval["interval"]) && $interval["interval"] == $maxIntervalNumber) {
+                array_push($maxIntervals, $interval);
             }
         }
 
-        return $maxInterval;
+        return $maxIntervals;
     }
 }
